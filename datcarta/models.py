9d6550a2d99ae2333a1a565c185d64643bb65147
Jeniffer-De <vivi-2222@hotmail.com>
2022-08-07 13:59:16 -0500

    Herencyviewsmode

--- datcarta/models.py
+++ datcarta/models.py
@@ -4,7 +4,7 @@ class Datcarta(models.Model):
     
     name = models.CharField(max_length=50)
     Cedula = models.FloatField()
-    Fecha_de_Ingreso = models.CharField(max_length=50)
+    Fecha_de_Ingreso = models.DateField(auto_now_add=True)
     Cargo = models.CharField(max_length=50)
     Salario = models.FloatField() 
     Tipo_de_contrato = models.CharField(max_length=50)   
--- datcarta/views.py
+++ datcarta/views.py
@@ -1,3 +1,18 @@
+from multiprocessing import context
 from django.shortcuts import render
+from datcarta.models import Datcarta
 
-# Create your views here.
+def crear_datos(request):
+    New_Usuario = Datcarta.objects.create(
+        name= "Julian Ospina Rendon",
+        cedula="1036734255",
+        Fecha_de_Ingreso = "15 agosto 2022",
+        Cargo = "Developer",
+        Salario = "2.500.000" ,
+        Tipo_de_contrato = "termino fijo"   
+        )
+    context = {
+        "New_Usuario" : New_Usuario
+    }
+    return render(request,"New_Usuario.html", context=context)
+    
