947136e9e89bf8951e07d5ca75b57cdaffbbe49a
Jeniffer-De <vivi-2222@hotmail.com>
2022-08-07 13:07:20 -0500

    Primermigrate

--- datcarta/models.py
+++ datcarta/models.py
@@ -3,8 +3,8 @@ from django.db import models
 class Datcarta(models.Model):
     
     name = models.CharField(max_length=50)
-    Cedula = models.Float()
-    Fecha_de_Ingreso = models.date()
+    Cedula = models.FloatField()
+    Fecha_de_Ingreso = models.CharField(max_length=50)
     Cargo = models.CharField(max_length=50)
     Salario = models.FloatField() 
     Tipo_de_contrato = models.CharField(max_length=50)   
