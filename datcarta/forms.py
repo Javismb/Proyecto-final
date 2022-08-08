daf48c127337f53bd7334bea82e5c409dfb1a92e
Javier Martinez Balmaceda <javiersmb83@gmail.com>
2022-08-08 05:46:47 -0300

    models, forms

---
+++ datcarta/forms.py
@@ -0,0 +1,10 @@
+from django import forms
+
+class new_user(forms.Form):
+
+    name = forms.CharField(max_length=40)
+    Cedula = forms.FloatField()
+    Cargo = forms.CharField(max_length=50)
+    salario = forms.FloatField() 
+    Tipo_de_contrato = forms.CharField(max_length=50)   
+   
--- datcarta/views.py
+++ datcarta/views.py
@@ -1,18 +1,29 @@
 from multiprocessing import context
 from django.shortcuts import render
 from datcarta.models import Datcarta
+from datcarta.forms import new_user
 
 def crear_datos(request):
-    New_Usuario = Datcarta.objects.create(
-        name= "Julian Ospina Rendon",
-        cedula="1036734255",
-        Fecha_de_Ingreso = "15 agosto 2022",
-        Cargo = "Developer",
-        Salario = "2.500.000" ,
-        Tipo_de_contrato = "termino fijo"   
-        )
-    context = {
-        "New_Usuario" : New_Usuario
-    }
-    return render(request,"New_Usuario.html", context=context)
     
+   if request.method == "POST":
+    pass
+    #New_Usuario = Datcarta.objects.create(
+        #name= "Julian Ospina Rendon",
+        #cedula="1036734255",
+        #Fecha_de_Ingreso = "15 agosto 2022",
+        #Cargo = "Developer",
+        #Salario = "2.500.000" ,
+        #Tipo_de_contrato = "termino fijo"   
+        #)
+    #context = {
+     #   "New_Usuario" : New_Usuario
+    #}
+   elif request.method == "GET":
+     form = new_user()
+     context = {'form':form}
+    
+     return render(request,"New_Usuario.html", context=context)
+
+
+
+
